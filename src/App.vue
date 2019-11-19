c970aacd15209c55995be83b8191faf4f7b8cbb2
zhs123123 <123123@qq.com>
2019-11-19 15:28:11 +0800

    element-ui整合

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <router-view></router-view>
+    <el-button type="success">成功按钮</el-button>
+  </div>
 </template>
 
 <script>
 export default {
-  name:'app'
-}
+  name: "app"
+};
 </script>
 
 <style>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+/* 导入element-ui */
+import ElementUI from 'element-ui';
+// 导入 Element-ui的样式
+import 'element-ui/lib/theme-chalk/index.css';
+// 注册 Element-ui
+Vue.use(ElementUI);
+
 //导入路由
 import router from './router/router.js'
 new Vue({
