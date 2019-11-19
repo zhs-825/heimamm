036f387a27e66d07a75f01d84f4a72c19a89d486
zhs123123 <123123@qq.com>
2019-11-19 15:41:26 +0800

    登录模块-整合路由

--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,17 @@
-//1.导入 vue-touter
-import VueRouter from 'vue-router';
 //导入Vue
 import Vue from 'vue'
+
+//1.导入 vue-touter
+import VueRouter from 'vue-router';
 //使用路由
 Vue.use(VueRouter);
+
+// 导入login路由
+import login from '../views/login/login.vue'
 //定义路由规则
-const routes=[]
+const routes=[
+    {path:'/login',component:login}
+]
 //创建路由实例
 const router=new VueRouter({
     routes
---
+++ src/views/login/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      这是登录页面
+  </div>
+</template>
+
+<script>
+export default {
+    name:'login'
+}
+</script>
+
+<style>
+
+</style>
