1d169ca7f1b68ff4740e063a4cc0c14386488c42
zhs123123 <123123@qq.com>
2019-11-22 11:57:07 +0800

    主页-路由整合

--- src/router/router.js
+++ src/router/router.js
@@ -8,9 +8,11 @@ Vue.use(VueRouter);
 
 // 导入login路由
 import login from '../views/login/login.vue'
+import index from '../views/index/index.vue'
 //定义路由规则
 const routes=[
-    {path:'/login',component:login}
+    {path:'/login',component:login},
+    {path:'/index',component:index}
 ]
 //创建路由实例
 const router=new VueRouter({
