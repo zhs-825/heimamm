51eabd554b1e7fbc006e23f931eccefa554545bb
zhs123123 <123123@qq.com>
2019-11-26 01:09:48 +0800

    列表渲染

--- src/views/index/subject/subject.vue
+++ src/views/index/subject/subject.vue
@@ -1,13 +1,137 @@
 <template>
-  <div>发顺丰速度快放假德生科技</div>
+  <div class="subject-container">
+    <!-- 头部 -->
+    <el-card>
+      <!-- 行内 表单 -->
+      <el-form :inline="true" :model="formInline" class="demo-form-inline">
+        <el-form-item label="学科编号">
+          <el-input v-model="formInline.rid"></el-input>
+        </el-form-item>
+        <el-form-item label="学科名称" class="more-width">
+          <el-input v-model="formInline.name"></el-input>
+        </el-form-item>
+        <el-form-item label="创建者">
+          <el-input v-model="formInline.creater"></el-input>
+        </el-form-item>
+        <el-form-item label="状态" class="more-width">
+          <el-select v-model="formInline.status" placeholder="请选择状态">
+            <el-option label="启用 " value="1"></el-option>
+            <el-option label="禁用" value="0"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="search">搜索</el-button>
+          <el-button>清除</el-button>
+          <el-button type="primary" icon="el-icon-plus">新增学科</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <!-- 内容 -->
+    <el-card class="main-card">
+      <!-- 表格 -->
+      <el-table :data="tableData" style="width: 100%" stripe border>
+        <el-table-column type="index" label="序号"> </el-table-column>
+        <el-table-column prop="rid" label="学科编号"> </el-table-column>
+        <el-table-column prop="name" label="学科名称"> </el-table-column>
+        <el-table-column prop="short_name" label="简称"> </el-table-column>
+        <el-table-column prop="creater" label="创建者"> </el-table-column>
+        <el-table-column prop="create_time" label="创建日期"> </el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+              <span v-if="scope.row.status===0" class="red">禁用</span>
+              <span v-else>启用 </span>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <!-- 插槽 -->
+          <template>
+            <el-button type="text">编辑</el-button>
+            <el-button type="text">禁用</el-button>
+            <el-button type="text">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页器 -->
+      <el-pagination
+        background
+        :current-page="1"
+        :page-sizes="[5, 10, 15, 20]"
+        :page-size="100"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="36"
+      >
+      </el-pagination>
+    </el-card>
+  </div>
 </template>
 
 <script>
+/* 导入接口方法 */
+import {subject} from '../../../api/api.js'
 export default {
-
-}
+  name: "subject",
+  data() {
+    return {
+      formInline: {},
+      //表格数据
+      tableData: [],
+      //页码
+      page:1,
+      //页容量
+      limit:10
+    };
+  },
+  //生命接口  data创建了
+  created() {
+    //1.axios方法使用
+    subject.list({
+      page:this.page,
+      limit:this.limit
+    }).then(res=>{
+      //成功回调
+      window.console.log(res)
+      //赋值给table
+       this.tableData=res.data.data.items
+    });
+  },
+  methods: {
+    search(){
+      //1.axios方法使用
+      subject.list({
+        page:this.page,limit:this.limit,...this.formInline
+      }).then(res=>{
+        //成功回调
+        window.console.log(res)
+        //赋值给table
+       this.tableData=res.data.data.items
+      });
+    }
+  },
+};
 </script>
 
-<style>
+<style lang="less">
+.subject-container {
+  .el-input__inner {
+    width: 100px;
+  }
+  .more-width {
+    .el-input__inner {
+      width: 150px;
+    }
+  } 
 
+  // 分页器的样式
+  .el-pagination{
+    text-align: center;
+    margin-top: 30px;
+  }
+  // card的样式
+  .main-card{
+    margin-top: 20px;
+    .red{
+      color:red;
+    }
+  }
+}
 </style>
