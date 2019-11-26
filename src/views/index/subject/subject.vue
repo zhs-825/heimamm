0a1ffe09ee553167b12af22d06d2d4a4605f8087
zhs123123 <123123@qq.com>
2019-11-27 00:53:02 +0800

    启用禁用切换功能

--- src/views/index/subject/subject.vue
+++ src/views/index/subject/subject.vue
@@ -22,7 +22,7 @@
         <el-form-item>
           <el-button type="primary" @click="search">搜索</el-button>
           <el-button>清除</el-button>
-          <el-button type="primary" icon="el-icon-plus">新增学科</el-button>
+          <el-button type="primary" icon="el-icon-plus" @click="dialogFormVisible=true">新增学科</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -30,24 +30,26 @@
     <el-card class="main-card">
       <!-- 表格 -->
       <el-table :data="tableData" style="width: 100%" stripe border>
-        <el-table-column type="index" label="序号"> </el-table-column>
-        <el-table-column prop="rid" label="学科编号"> </el-table-column>
-        <el-table-column prop="name" label="学科名称"> </el-table-column>
-        <el-table-column prop="short_name" label="简称"> </el-table-column>
-        <el-table-column prop="creater" label="创建者"> </el-table-column>
-        <el-table-column prop="create_time" label="创建日期"> </el-table-column>
+        <el-table-column type="index" label="序号"></el-table-column>
+        <el-table-column prop="rid" label="学科编号"></el-table-column>
+        <el-table-column prop="name" label="学科名称"></el-table-column>
+        <el-table-column prop="short_name" label="简称"></el-table-column>
+        <el-table-column prop="creater" label="创建者"></el-table-column>
+        <el-table-column prop="create_time" label="创建日期"></el-table-column>
         <el-table-column label="状态">
           <template slot-scope="scope">
-              <span v-if="scope.row.status===0" class="red">禁用</span>
-              <span v-else>启用 </span>
+            <span v-if="scope.row.status===0" class="red">禁用</span>
+            <span v-else>启用</span>
           </template>
         </el-table-column>
         <el-table-column label="操作">
           <!-- 插槽 -->
-          <template>
+          <template slot-scope="scope">
             <el-button type="text">编辑</el-button>
-            <el-button type="text">禁用</el-button>
-            <el-button type="text">删除</el-button>
+            <el-button type="text" @click="status(scope.row)">
+              {{scope.row.status==1?'禁用':'启用'}}
+            </el-button>
+            <el-button type="text" @click="remove(scope.row)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -61,79 +63,191 @@
         :total="total"
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
-      >
-      </el-pagination>
+      ></el-pagination>
     </el-card>
+    <!-- 新增对话框 -->
+    <el-dialog title="新增学科" :visible.sync="dialogFormVisible">
+      <el-form :model="addForm" :rules="rules" ref="addForm">
+        <el-form-item label="学科编号" :label-width="formLabelWidth" prop="rid">
+          <el-input v-model="addForm.rid" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="学科名称" :label-width="formLabelWidth" prop="name">
+          <el-input v-model="addForm.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="学科简称" :label-width="formLabelWidth">
+          <el-input v-model="addForm.short_name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="学科简介" :label-width="formLabelWidth">
+          <el-input type="textarea" :row="2" v-model="addForm.intro" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="学科备注" :label-width="formLabelWidth">
+          <el-input v-model="addForm.remark" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitAdd">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 /* 导入接口方法 */
-import {subject} from '../../../api/api.js'
+import { subject } from "../../../api/api.js";
 export default {
   name: "subject",
   data() {
     return {
-      formInline: {},
+      //筛选的表格
+      formInline: [],
       //表格数据
       tableData: [],
       //页码
-      page:1,
+      page: 1,
       //页容量
-      limit:10,
+      limit: 10,
       //页面数组
-      pageSize:[5, 10, 15, 20],
+      pageSize: [5, 10, 15, 20],
       //总条数
-      total:0
+      total: 0,
+
+      /* 新增表单数据 */
+      addForm: {},
+      //新增表单是否显示
+      dialogFormVisible: false,
+      //表单左边文字lable的宽度 不设置的话不能再同一行
+      formLabelWidth: "100px",
+      //表单验证规则
+      rules: {
+        rid: [{ required: true, message: "学科编号不能为空" }],
+        name: [{ required: true, message: "学科名称不能为空" }]
+      }
     };
   },
   //生命接口  data创建了
   created() {
     //1.axios方法使用
-    subject.list({
-      page:this.page,
-      limit:this.limit
-    }).then(res=>{
-      //成功回调
-      window.console.log(res)
-      //赋值给table
-       this.tableData=res.data.data.items
-      // 保存 总条数
+    subject
+      .list({
+        page: this.page,
+        limit: this.limit
+      })
+      .then(res => {
+        //成功回调
+        window.console.log(res);
+        //赋值给table
+        this.tableData = res.data.data.items;
+        // 保存 总条数
         this.total = res.data.data.pagination.total;
-    });
+      });
   },
   methods: {
-    getList(){
-      //数据请求 
-      subject.list({
-        page:this.page,limit:this.limit,...this.formInline
-      }).then(res=>{
-        //成功回调
-        window.console.log(res)
+    getList() {
+      //数据请求
+      subject
+        .list({
+          page: this.page,
+          limit: this.limit,
+          ...this.formInline
+        })
+        .then(res => {
+          //成功回调
+          window.console.log(res);
 
-        //赋值给table
-       this.tableData=res.data.data.items
-       //保存总条数的值
-       this.total=res.data.data.pagination.total
+          //赋值给table
+          this.tableData = res.data.data.items;
+          //保存总条数的值
+          this.total = res.data.data.pagination.total;
+        });
+    },
+    search() {
+      //1.axios方法使用
+      this.page = 1;
+      this.getList();
+    },
+    //页容量改变
+    handleSizeChange(size) {
+      this.page = 1;
+      this.limit = size;
+      this.getList();
+    },
+    //页码改变
+    handleCurrentChange(current) {
+      this.page = current;
+      this.getList();
+    },
+    //提交新增表单
+    submitAdd() {
+      this.$refs.addForm.validate(valid => {
+        if (valid) {
+          //新增成功
+          //调用接口
+          //1.axios方法使用
+          subject.add(this.addForm).then(res => {
+            //成功回调
+            window.console.log(res);
+
+            if (res.data.code == 200) {
+              this.dialogFormVisible = false;
+              this.$message.success(res.data.message);
+              // 重新获取一次
+              this.getList();
+            }
+          });
+        } else {
+          //失败
+          this.$message.warning("小老弟");
+          return false;
+        }
       });
     },
-    search(){
+    //点击删除事件
+    remove(data) {
+      window.console.log(data);
+      this.$confirm("此操作将永久删除, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          subject
+            .remove({
+              id: data.id
+            })
+            .then(res => {
+              //成功回调
+              window.console.log(res);
+              if(res.data.code==200){
+                this.$message.success(res.data.message)
+                this.getList()
+              }
+            });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
       //1.axios方法使用
-      this.page=1;
-      this.getList()
     },
-    //页容量改变 
-    handleSizeChange(size){
-      this.page=1
-      this.limit=size
-      this.getList()
-    },
-    //页码改变  
-    handleCurrentChange(current){
-      this.page=current
-      this.getList()
+    //启用禁用切换
+    status(data){
+      //1.axios方法使用
+      subject.status({
+        id:data.id,
+        status:data.status==1?0:1
+      }).then(res=>{
+        //成功回调
+        // window.console.log(res)
+        if(res.data.code==200){
+          this.$message.success(res.data.message);
+          this.getList()
+        }
+      });
     }
-  },
+  }
 };
 </script>
 
@@ -146,19 +260,35 @@ export default {
     .el-input__inner {
       width: 150px;
     }
-  } 
+  }
 
   // 分页器的样式
-  .el-pagination{
+  .el-pagination {
     text-align: center;
     margin-top: 30px;
   }
   // card的样式
-  .main-card{
+  .main-card {
     margin-top: 20px;
-    .red{
-      color:red;
+    .red {
+      color: red;
     }
+  }
+}
+/* 新增对话框 */
+.el-dialog {
+  height: 510px;
+  .el-dialog__header {
+    background: linear-gradient(360deg, blue, skyblue, yellow, pink);
+    text-align: center;
+    line-height: 30px;
+    .el-dialog__title {
+      color: white;
+      font-weight: bold;
+    }
+  }
+  .el-input__inner {
+    width: 100%;
   }
 }
 </style>
